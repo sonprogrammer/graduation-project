543edff74ff51ef3fa7caf21fc47231e711d8ab5
손영진 <ods04138@gmail.com>
2023-04-04 16:46:36 +0900

    기초세팅

---
+++ Desktop/대학교/졸프(4-1)/project/SchoolBus/src/main/java/com/example/SchoolBus/SchoolBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchoolBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBusApplication.class, args);
+	}
+
+}
---
+++ Desktop/대학교/졸프(4-1)/project/SchoolBus/src/test/java/com/example/SchoolBus/SchoolBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SchoolBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
