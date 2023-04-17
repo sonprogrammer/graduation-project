0596ce8afde84381769a02703cb09c11df1bcd8d
조용훈 <sjsin0905@naver.com>
2023-04-18 00:32:16 +0900

    BoardTest
    클래스만 만들었음

---
+++ SchoolBus/src/test/java/com/example/SchoolBus/BoardTest.java
@@ -0,0 +1,20 @@
+package com.example.SchoolBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardTest {
+    @Test
+    void AddPost(){
+
+    }
+    @Test
+    void DeletePost(){
+
+    }
+    @Test
+    void ModifyPost(){
+
+    }
+}
