26e01f8ed2ad926e210ddd44248cf51aed160017
조용훈 <sjsin0905@naver.com>
2023-04-19 10:19:49 +0900

    getCurrentPosition() --> watchPosition()
    getCurrentPosition() --> watchPosition()
    수정 하였음

--- SchoolBus/src/main/java/com/example/SchoolBus/controller/map_controller.java
+++ SchoolBus/src/main/java/com/example/SchoolBus/controller/map_controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class map_controller {
     @GetMapping("/map_manager")
     public String mapaction (){
-        return "map_manager";
+        return "map_manager_test";
     }
     @GetMapping("/map_user")
     public String mapaction2 (){
