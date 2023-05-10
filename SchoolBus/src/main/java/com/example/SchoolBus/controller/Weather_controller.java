c6c9f109f20e263df834b44e6d4629291795b7f2
조용훈 <sjsin0905@naver.com>
2023-05-10 14:10:12 +0900

    test

--- SchoolBus/src/main/java/com/example/SchoolBus/controller/Weather_controller.java
+++ SchoolBus/src/main/java/com/example/SchoolBus/controller/Weather_controller.java
@@ -2,20 +2,17 @@ package com.example.SchoolBus.controller;
 
 import com.example.SchoolBus.Weather_Crawling.Weather_cra;
 import com.example.SchoolBus.Weather_Crawling.Weather_obj;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-
+@RequiredArgsConstructor
 @Controller
 public class Weather_controller {
-//    private final Weather_cra weatherCra;
-//
-//    public Weather_controller(Weather_cra weatherCra) {
-//        this.weatherCra = weatherCra;
-//    }
+    private final Weather_cra weatherCra1;
 
     @GetMapping("/timetable")
     public String timetable(Model model) throws IOException {
@@ -27,8 +24,8 @@ public class Weather_controller {
         String test1 = weatherCra1.getTest1();
 
 
-        model.addAttribute("rainfall", rainfall);
-        model.addAttribute("weather", weather);
+        model.addAttribute("rainfall", weatherCra1.getRainfall());
+        model.addAttribute("weather", weatherCra1.getWeather());
         model.addAttribute("test1", test1);
         model.addAttribute("test2", test2);
 
